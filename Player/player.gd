bad08777699a0bddf7942a6540fb251ec15eeeb4
Harsh Gajrani <harshgajrani23@gmail.com>
2026-01-13 22:36:44 +0530

    Refactor player animations and adjust player position in the world scene

--- Player/player.gd
+++ Player/player.gd
@@ -4,10 +4,11 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var anim = get_node("AnimatedSprite2D");
+@onready var anim = get_node("AnimationPlayer");
 
 func _ready() -> void:
 	anim.play("Idle")
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -16,15 +17,18 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		anim.play("Jump")
+	if velocity.y > 0:
+		anim.play("Fall")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-		anim.play("Run")
+		if velocity.y == 0:
+			anim.play("Run")
 	else:
-		anim.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if velocity.y == 0:
+			anim.play("Idle")
 	move_and_slide()
