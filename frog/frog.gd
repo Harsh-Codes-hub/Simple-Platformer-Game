3f0d6da9911cda9f7e53d3425839aa29e71ffcff
Harsh Gajrani <harshgajrani23@gmail.com>
2026-02-02 23:31:33 +0530

    Fixed logic of frog mob

--- frog/frog.gd
+++ frog/frog.gd
@@ -1,51 +1,85 @@
 extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var SPEED = 50
-var player
-var chase = false
+var SPEED := 50.0
+var chase := false
+var is_dead := false
+
+@onready var anim := $AnimatedSprite2D
+
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("Idle")
+	anim.play("Idle")
+
+
 func _physics_process(delta: float) -> void:
+	if is_dead:
+		velocity.x = 0
+		move_and_slide()
+		return
+
 	velocity.y += gravity * delta
-	if (chase == true):
-		if $AnimatedSprite2D.animation != "Death":
-			$AnimatedSprite2D.play("Jump")
-		player = $"../../player/player"
-		var direction = (player.position - self.position).normalized()
-		if (direction.x > 0):
-			$AnimatedSprite2D.flip_h = true
-		else:
-			$AnimatedSprite2D.flip_h = false
+
+	if chase:
+		if anim.animation != "Death":
+			anim.play("Jump")
+
+		var player := $"../../player/player"
+		var direction = (player.position - position).normalized()
+
+		anim.flip_h = direction.x > 0
 		velocity.x = direction.x * SPEED
 	else:
-		if $AnimatedSprite2D.animation != "Death":
-			$AnimatedSprite2D.play("Idle")
+		if anim.animation != "Death":
+			anim.play("Idle")
 		velocity.x = 0
+
 	move_and_slide()
 
-func _on_player_detection_body_entered(body):
+
+# --- Detection ---
+func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		chase = true
+
 
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	if body.name == "player":
 		chase = false
 
 
+# --- Head stomp ---
 func _on_player_death_body_entered(body: Node2D) -> void:
-	if body.name == "player":
+	if is_dead:
+		return
+	if body.name != "player":
+		return
+
+	# Player must be ABOVE the frog
+	if body.global_position.y < global_position.y:
 		death()
 
 
+# --- Side / bottom damage ---
 func _on_player_collison_body_entered(body: Node2D) -> void:
+	if is_dead:
+		return
 	if body.name == "player":
 		body.health -= 3
 		death()
 
+
+# --- Death ---
 func death():
-		chase = false
-		$AnimatedSprite2D.play("Death")
-		await $AnimatedSprite2D.animation_finished
-		self.queue_free()
+	if is_dead:
+		return
+
+	is_dead = true
+	chase = false
+
+	# 🔥 THIS LINE FIXES THE INVISIBLE PLATFORM
+	$CollisionShape2D.set_deferred("disabled", true)
+
+	$AnimatedSprite2D.play("Death")
+	await $AnimatedSprite2D.animation_finished
+	queue_free()
